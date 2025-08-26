6246c34bcd77805083f042bbc32af647e0109600
Yminnnnn <165258300+Yminnnnn@users.noreply.github.com>
2025-08-26 15:50:04 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import Header from "./components/Header";
-import Hero from "./sections/Hero";
-import Brand from "./sections/Brand";
-import Products from "./sections/Products";
-import Reviews from "./sections/Reviews";
-import Cta from "./sections/Cta";
-import Footer from "./components/Footer";
-import "./styles/main.scss";
+function App() {
+  const [count, setCount] = useState(0)
 
-export default function App() {
   return (
     <>
-      <Header />
-      <main>
-        <section id="hero" className="Section"><Hero /></section>
-        <section id="brand" className="Section"><Brand /></section>
-        <section id="products" className="Section"><Products /></section>
-        <section id="reviews" className="Section"><Reviews /></section>
-        <section id="contact" className="Section"><Cta /></section>
-      </main>
-      <Footer />
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
+
+export default App
