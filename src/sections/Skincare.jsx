ec4d115deb6f7bb3f6fa441fca4044787222f3d5
hym-dot <yumin6350@kbu.ac.kr>
2025-09-04 21:41:15 +0900

    .

--- src/sections/Skincare.jsx
+++ src/sections/Skincare.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import skincare from '../util/skincare'
 
 const Skincare = () => {
   return (
-    <div>Skincare</div>
+    <div className='inner skincare-inner'>
+      <div className="t-wrap">
+
+        <h2 className="tit" >
+          Vegan Skincare
+        </h2>
+        <p className="txt">
+          피부에 건강한 영향력을 전달하고 꼭 필요한 성분만을 담아 <br />
+          놀라운 변화를 선사하는 비건 스킨케어 브랜드 토코보
+        </p>
+        <a href="#" className="btn">
+          MEET TOCOBO
+        </a>
+      </div>
+    </div>
   )
 }
 
